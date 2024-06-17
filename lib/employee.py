012736dfbf3f5649f42f5e0446c0c4356057cc23
Guang <jouyang19@proton.me>
2024-06-16 20:10:02 -0400

    complete lab

--- lib/employee.py
+++ lib/employee.py
@@ -19,6 +19,39 @@ class Employee:
             f"<Employee {self.id}: {self.name}, {self.job_title}, " +
             f"Department ID: {self.department_id}>"
         )
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str) and len(new_name):
+            self._name = new_name
+        else:
+            raise ValueError('Name must be a non-empty string')
+        
+    @property
+    def job_title(self):
+        return self._job_title
+    
+    @job_title.setter
+    def job_title(self, job_title):
+        if isinstance(job_title, str) and len(job_title):
+            self._job_title = job_title
+        else:
+            raise ValueError('Job title must be a non-empty string')
+        
+    @property
+    def department_id(self):
+        return self._department_id
+    
+    @department_id.setter
+    def department_id(self, department_id):
+        if type(department_id) is int and Department.find_by_id(department_id):
+            self._department_id = department_id
+        else:
+            raise ValueError('Department ID must reference an existing department in the database')
 
     @classmethod
     def create_table(cls):
